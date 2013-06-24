a29d6873a64a2ec9f2774731f873310ec06b65a3
hckjsnzf <hckjsnzf@gmail.com>
2013-06-24 23:01:12 +0800

    add MACRO and TYPE

--- mfr_conf.h
+++ mfr_conf.h
@@ -9,7 +9,7 @@
 #define __MFR_CONF_H__
 
 #define ACCLENG 20
-#define BUFLENG 100
+#define BUFLENG 200
 #define TIMLENG 15
 /* 20130506153000 */
 #define AMTLENG 2
@@ -31,23 +31,68 @@ typedef struct mfrst_acc
 }MFR_ACC, *PMFR_ACC;
 
 
-#define SPENT '0'
-#define EARNED '1'
-#define BLACKCASH '2'
-#define TRANSFER  '3'
+#define T_SPENT     '0'
+#define T_EARNED    '1'
+#define T_BLACKCASH '2'
+#define T_RIGHTCASH '3'
+#define T_TRANSFER  '4'
 
+
+#define U_EATING    '0'
+#define U_CLOTHES   '1'
+#define U_HOUSE     '2'
+#define U_BOOK      '3'
+#define U_PUBBILL   '4'
+#define U_TRAFIC    '5'
+#define U_IT3C      '6'
+#define U_SPORTS    '7'
+#define U_OTHER     '8'
+
+
+#define TA_EBREAKF   '1'
+#define TA_ELUNCH    '2'
+#define TA_EDINNER   '3'
+#define TA_CJACKET   '4'
+#define TA_CPANTS    '5'
+#define TA_CSHOOES   '6'
+#define TA_BPROGRAM  '7'
+#define TA_BSINOLOGY '8'
+#define TA_HBILLS    '9'
+#define TA_HTHING    'a'
+#define TA_PWATER    'b'
+#define TA_PELECT    'c'  
+#define TA_PGAS      'd'
+#define TA_TBUS      'e'
+#define TA_TTAXI     'f'
+#define TA_TSUBW     'g'
+#define TA_ICOMPU    'h'
+#define TA_IKEYBO    'i'
+#define TA_IMOUSE    'j'
+#define TA_IHADRD    'k'
+#define TA_IOTHER    'l'
+#define TA_SBUY      'm'
+#define TA_SYARD     'n'
+#define TA_OTHER     'o'
 
 
 typedef struct mfrst_move
 {
 	char mvdt    [TIMLENG];
-	char mvtype  ; /* SPENT, EARNED , BLACKCASH , TRANSFER */
+	int  dayindex;
+	char mvtype  ; /* SPENT, EARNED , BLACKCASH , RIGHTCASH, TRANSFER */
 	char mvfacc  [ACCLENG];
 	char mvtacc  [ACCLENG];
 	
-	char mvuse   ; /* EATING, BOOK, PUBBILL, TRAFIC,  */
+	char mvuse   ; /* EATING, CLOTHES, HOUSE, BOOK, PUBBILL, TRAFIC, IT3C, SPORTS, OTHER */
 	char mvtags  [TAGLENG];
 	int  tagnum  ;
+	char inbuf   [BUFLENG];
+	char indate  [TIMLENG];
+}MFR_MOV, *PMFR_MOV;
+
+
+
+	
 	
 	
 	
